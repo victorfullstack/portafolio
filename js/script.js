748e77d16bf47f054abbc2621c93f8db1aec2184
victorfullstack <victor0321007@gmail.com>
2024-01-29 20:08:46 -0500

    se agrego responsivo y animacion con css javaScript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 //toggle icon navbar//
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
@@ -35,4 +42,9 @@ window.onscroll = () =>{
     // let header = document.querySelectorAll('header');
 
     // header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
+
